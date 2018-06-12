3b7801011bd2ae9a208549d3e0e00d959f1f6d35
Nicholas Boutboul <boutboulnicholas@gmail.com>
2018-06-11 22:03:21 -0700

    added tests and change possibilities still needs work

---
+++ changePossibilities.py
@@ -0,0 +1,30 @@
+from collections import Counter
+from copy import copy
+
+# This still needs some work and doesen't work entirely, I will keep updating it when I get it to work
+# with any permutations... will work on it
+def changePossibilities(amount, denom):
+    posib = 0
+    alredy = []
+    denom = list(set(denom))
+    for d in denom:
+        if d == amount:
+            posib += 1
+            continue;
+        elif d > amount:
+          continue;
+        to = [d]
+        for test in denom:
+            while sum(to) < amount:
+                to.append(test)
+                if sum(to) == amount and notAlredy(alredy, to):
+                    alredy.append(copy(to))
+                    posib += 1
+            del to[1:]
+    return posib
+
+def notAlredy(a, c):
+  for l in a:
+    if Counter(l) == Counter(c):
+      return False
+  return True
--- decodeString.py
+++ decodeString.py
@@ -1,5 +1,8 @@
 import re
 
+# This took a little while but after a friend talked about eval it made sense to me, I was
+# going for a recursive function at first since I had the regex working but it was hard
+# after thinking about eval and pythons ability to do 5 * str it became evident and i filled in the gap
 def decodeString(s):
     matches = re.findall(r'\d{1}\[[a-zA-Z]+', s)
     finalArr = []
